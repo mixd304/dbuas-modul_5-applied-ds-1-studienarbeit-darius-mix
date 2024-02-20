00b169075280a5788f37e3ab9a86b08eff5e688f
Darius Mix <darius.mix@gmx.de>
2024-02-20 19:53:36 +0100

    update eda

--- lib_py/eda.py
+++ lib_py/eda.py
@@ -11,6 +11,25 @@ from dwh import conn
 
 c = conn.cursor()
 
+def get_events():
+    query = ''' 
+        SELECT event AS Event
+	        ,game AS Game
+	        ,month AS Month
+        FROM DIM_Event
+    '''
+    return pd.read_sql_query(query, con=conn)
+
+def get_filterwords_count_total_by_day():
+    query = ''' 
+        SELECT date as Day
+	        ,word AS Word
+	        ,sum(count) AS Count
+        FROM count_filter
+        GROUP BY date, word
+    '''
+    return pd.read_sql_query(query, con=conn)
+
 def get_filterwords_count_total_by_month():
     query = ''' 
         SELECT substr(date,0,8) as Month
@@ -52,6 +71,17 @@ def get_filterwords_count_total_by_paper_group_and_month():
         FROM count_filter
         LEFT JOIN DIM_Paper on count_filter.paper_id = DIM_Paper.id
         GROUP BY substr(count_filter.date,0,8), count_filter.word, DIM_Paper.Format
+    '''
+    return pd.read_sql_query(query, con=conn)
+
+def get_filterwords_word_pair_count_total():
+    query = ''' 
+        SELECT filter_word
+            ,other_word
+            ,sum(count) AS Count
+        FROM count_word_pair
+        GROUP BY filter_word, other_word
+        ORDER BY sum(count) DESC
     '''
     return pd.read_sql_query(query, con=conn)
 
@@ -118,8 +148,39 @@ def delete_filter_words(word_list):
     print(query)
     c.execute(query)
 
-def update_filter_words(new_word, word_list):
-    query = "UPDATE count_filter SET word = {} WHERE ".format(new_word) + \
+    query = "DELETE FROM count_word_pair WHERE " + \
+        " OR ".join(["filter_word LIKE '%{}%'".format(word) for word in word_list])
+    print(query)
+    c.execute(query)
+
+    query = "DELETE FROM word_with_prev_and_next_ten WHERE " + \
         " OR ".join(["word LIKE '%{}%'".format(word) for word in word_list])
     print(query)
     c.execute(query)
+
+    conn.commit()
+
+def update_filter_words(new_word, word_list):
+    query = "UPDATE count_filter SET word = '{}' WHERE ".format(new_word) + \
+        " OR ".join(["word LIKE '%{}%'".format(word) for word in word_list])
+    print(query)
+    c.execute(query)
+
+    query = "UPDATE count_word_pair SET filter_word = '{}' WHERE ".format(new_word) + \
+        " OR ".join(["filter_word LIKE '%{}%'".format(word) for word in word_list])
+    print(query)
+    c.execute(query)
+
+    query = "UPDATE word_with_prev_and_next_ten SET word = '{}' WHERE ".format(new_word) + \
+        " OR ".join(["word LIKE '%{}%'".format(word) for word in word_list])
+    print(query)
+    c.execute(query)
+
+    conn.commit()
+
+def update_games(new_word, games_list):
+    query = "UPDATE count_games SET game = '{}' WHERE ".format(new_word) + \
+        " OR ".join(["game LIKE '%{}%'".format(game) for game in games_list])
+    print(query)
+    c.execute(query)
+    conn.commit()
